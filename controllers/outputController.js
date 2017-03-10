400536e1dc93a3757ae47f53e29ebbe8c93ed03e
kambehba <kamparsen@yahoo.com>
2017-03-10 12:44:21 -0500

    sdf

--- controllers/outputController.js
+++ controllers/outputController.js
@@ -10,8 +10,8 @@ module.exports = function(app){
     
     //http GET
     
-    app.get('/api/outputs',function(req,res)
-    //app.get('https://internet-control.herokuapp.com/api/outputs',function(req,res)
+    //app.get('/api/outputs',function(req,res)
+    app.get('https://internet-control.herokuapp.com/api/outputs',function(req,res)
     {
         res.send('**************');
         Outputs.find({},function(err,outputs)
